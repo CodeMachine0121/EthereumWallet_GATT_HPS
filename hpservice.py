712cec7fffc29815da4747cd1511582bef8ee655
CodeMachine0121 <46839484+CodeMachine0121@users.noreply.github.com>
2019-07-15 07:38:42 +0800

    Update hpservice.py

--- hpservice.py
+++ hpservice.py
@@ -85,31 +85,32 @@ class HttpHeadersChrc(Characteristic):
 class HttpEntityBodyChrc(Characteristic):
     CHRC_UUID = '00002ab9-0000-1000-8000-00805f9b34fb'
     # put the response in to EnityBody
-    body=''
+    body = dict()
     def __init__(self):
         Characteristic.__init__(self, {
             'uuid': '2AB9',
             'properties': ['read'],
             'value': None }
             )
-        self.body = str()
+        
     
     def set_http_entity_body(self,value):
         print('body value you set: ',value)
         self.body = value
     
-    def get_http_entity_body(self):
-        return self.body
-
+   
     
     def onReadRequest(self, offset, callback):
         print('Body read: ',self.body)
-        callback(Characteristic.RESULT_SUCCESS, self.body[offset:])
+        if self.body == dict(): 
+            callback(Characteristic.RESULT_SUCCESS, ''.encode('utf8') )
+        else:
+            callback(Characteristic.RESULT_SUCCESS, self.body['response'].encode('utf8') )
 
 
 
 http_uriService = UriChrc()
-http_entity_body_chrc = HttpEntityBodyChrc()
+
 
 
 
@@ -119,21 +120,18 @@ class HttpControlPointChrc(Characteristic):
     # control application to request the API by methods
     
     CHRC_UUID = '00002aba-0000-1000-8000-00805f9b34fb'
-    response=''
+    response=dict()
     def __init__(self):
         Characteristic.__init__(self, {
             'uuid': '2ABA',
-            'properties': ['read','write'],
+            'properties': ['write'],
             'value': None }
             )
-    def onReadRequest(self,offset,callback):
-        print('Response: ',self.response)
-        callback(Characteristic.RESULT_SUCCESS, self.response.encode('utf8'))
 
-
+    
     def onWriteRequest(self, data, offset, withoutResponse, callback):
         types = int(str(data.decode()))
-        global to_Address , transactions
+        global to_Address , transactions , http_entity_body_chrc
 
         if types < 1 or types > 11:
             raise FailedException("0x80")
@@ -143,16 +141,18 @@ class HttpControlPointChrc(Characteristic):
         else:
             if types == 1:
                 print('GET')
-                self.GET_request()
+                status_code = self.GET_request()
                 to_Address = ''
                 transactions=''
             elif types == 3:
                 print('POST')
-                self.POST_request(to_Address,transactions)
+                status_code = self.POST_request(to_Address,transactions)
                 to_Address = ''
                 transactions=''
             else:
                 print('wrong type')
+            HttpEntityBodyChrc.set_http_entity_body(HttpEntityBodyChrc,self.response)
+            HttpStatusCodeChrc.set_http_status_code(HttpStatusCodeChrc,status_code)
         callback(Characteristic.RESULT_SUCCESS)
 
     def GET_request(self):
@@ -160,10 +160,8 @@ class HttpControlPointChrc(Characteristic):
         global http_uriService
         uri = http_uriService.getUri()
         r = requests.get(uri)
-        self.response = r.text
-
-       
-
+        self.response = r.json()
+        return r.status_code 
     def POST_request(self,to_Address,transactions):
         import requests
         global http_uriService
@@ -172,21 +170,21 @@ class HttpControlPointChrc(Characteristic):
         payload = to_Address+','+transactions
         datas = {'data':payload}
         r = requests.post( uri, data=datas)
-        self.response = r.text
-        
+        self.response = r.json()
+        return r.status_code 
 
 class HttpStatusCodeChrc(Characteristic):
 
     CHRC_UUID = '00002ab8-0000-1000-8000-00805f9b34fb'
     STATUS_BIT_BODY_RECEIVED = 4
+    http_status_code = 0
     def __init__(self):
         Characteristic.__init__(self, {
             'uuid': '2AB8',
             'properties': ['read'],
             'value': None }
             )
-        self.http_status_code = 0
-        self.set_http_status_code(self.http_status_code)
+        
         
 
     def set_http_status_code(self, value):
